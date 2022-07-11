d9823e96d9a6b68ea5b4c44447f7d5196dc0985a
kalypa <80308782+kalypa@users.noreply.github.com>
2022-07-11 19:55:18 +0900

    스태미나 바 회복&속도 증가, 타워 파괴 및 생성

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Slider staminaBar;
+    [SerializeField]
+    private StaminaBar stamina;
+    [SerializeField]
+    private TowerSpawner towerSpawner;
     DragManager dragManager;
-
     void Awake()
     {
         dragManager = GetComponent<DragManager>();
@@ -18,10 +24,34 @@ public class Player : MonoBehaviour
             if(Mathf.Abs(swipeDirection.x) > Mathf.Abs(swipeDirection.y))
             {
                 Debug.Log("가로베기");
+                if(towerSpawner.towerList[0].GetComponentInChildren<SlashDir>().transform.eulerAngles != new Vector3(0, 0, 0))
+                {
+                    PoolManager.ReturnObject(towerSpawner.towerList[0]);
+                    towerSpawner.towerList.Remove(towerSpawner.towerList[0]);
+                    staminaBar.value -= 1;
+                    stamina.Spd += 0.01f;
+                    --towerSpawner.count;
+                    var tower = PoolManager.GetObject();
+                    towerSpawner.towerList.Add(tower);
+                    tower.transform.position = new Vector3(towerSpawner.towerList[towerSpawner.count].transform.position.x, towerSpawner.towerList[towerSpawner.count].transform.position.y + 2);
+                    ++towerSpawner.count;
+                }
             }
             else if(Mathf.Abs(swipeDirection.y) > Mathf.Abs(swipeDirection.x))
             {
                 Debug.Log("세로베기");
+                if (towerSpawner.towerList[0].GetComponentInChildren<SlashDir>().transform.eulerAngles == new Vector3(0, 0, 0))
+                {
+                    PoolManager.ReturnObject(towerSpawner.towerList[0]);
+                    towerSpawner.towerList.Remove(towerSpawner.towerList[0]);
+                    staminaBar.value -= 1;
+                    stamina.Spd += 0.01f;
+                    --towerSpawner.count;
+                    var tower = PoolManager.GetObject();
+                    towerSpawner.towerList.Add(tower);
+                    tower.transform.position = new Vector3(towerSpawner.towerList[towerSpawner.count].transform.position.x, towerSpawner.towerList[towerSpawner.count].transform.position.y + 2);
+                    ++towerSpawner.count;
+                }
             }
         }
     }
--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class TowerSpawner : MonoBehaviour
 {
-    private float count = 0;
-    private float height = 3;
+    private SlashDir slashDir;
+    private float randomDir;
+    public int count = 0;
+    public float height = 3.1f;
+    public List<Tower> towerList = new List<Tower>();
     void Start()
     {
         var tower = PoolManager.GetObject();
+        towerList[count] = tower;
+        slashDir = tower.GetComponentInChildren<SlashDir>();
+        Spawn();
     }
 
     void Update()
     {
-        Spawn();
+        Debug.Log(count);
     }
     void Spawn()
     {
         while(count < 10)
         {
+            randomDir = Random.Range(1, 100);
             var tower = PoolManager.GetObject();
+            ++count;
+            towerList[count] = tower;
+            slashDir = tower.GetComponentInChildren<SlashDir>();
             var position = new Vector3(tower.transform.position.x, tower.transform.position.y + height);
             tower.transform.position += position;
-            ++count;
-            height += 3;
+            if (randomDir < 50)
+            {
+                slashDir.transform.eulerAngles = new Vector3(0, 0, 90);
+            }
+            height += 3.1f;
         }
     }
 }
